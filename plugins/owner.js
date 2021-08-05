a53d70e179c6e2b350fd69bf48b5b057418c3a60
Thoma <88131790+THOMAACHAYAN@users.noreply.github.com>
2021-08-06 00:52:34 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -16,7 +16,7 @@ if (Config.WORKTYPE == 'private') {
 
     
 
-    r_text[1] = "*╔═════😎THOMA😎═════╗*\n           \n*⚜═THOMA═⚜*\n\n*owner MR FRAUD LFA- http://Wa.me/+919562354752*\n* *\n🔰instagram:- https://youtube.com/c/ICHUTECH*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: MR FRAUD LFA*"
+    r_text[1] = "*╔═════😎THOMA😎═════╗*\n           \n*⚜═THOMA═⚜*\n\n*owner MR FRAUD LFA- http://Wa.me/+919562354752*\n*    *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: MR FRAUD LFA*"
 
     
 
@@ -56,7 +56,7 @@ if (Config.WORKTYPE == 'private') {
 
     
 
-    r_text[1] = "*╔═════😎THOMA😎═════╗*\n           \n*⚜═THOMA═⚜*\n\n*owner MR FRAUD LFA- http://Wa.me/+919562354752*\n* *\n🔰Youtube: https://youtube.com/c/ICHUTECH*            *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: MR FRAUD LFA*"
+    r_text[1] = "*╔═════😎THOMA😎═════╗*\n           \n*⚜═THOMA═⚜*\n\n*owner MR FRAUD LFA- http://Wa.me/+919562354752*\n*             *\n*╚══════🔱🔱🔱🔱🔱═════╝*\n\n*▷Creator: MR FRAUD LFA*"
 
     
 
